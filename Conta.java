0ef5669fe3869a07217212158902e16ebeacbde2
Aécio Guterres <aecioguterres.dev@gmail.com>
2023-06-08 15:56:32 -0300

    Criação do Atributo numeroAgencia e Método TransferirValor

--- Conta.java
+++ Conta.java
@@ -2,9 +2,13 @@ class Conta{
 	
 //Atributos da classe conta
         String numeroConta;
+        public Long numeroAgencia;
 
 //Métodos da classe ser conta
         public void sacarValor(){
 
+        }
+        
+        public void transferirValor(){
         }
 }
