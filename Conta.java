9dd81d239fd95e9b74b2fbf78ba2dd02f963d7ed
taniacruzz <tanicruz112@gmail.com>
2023-06-11 02:49:22 -0300

    exclusão de lógica métodos sacarValor, trasnferirValor, simularRendimentos

--- Conta.java
+++ Conta.java
@@ -10,12 +10,10 @@ class Conta{
 
 //Métodos da classe ser conta
         public void sacarValor(Double valorSaque){
-                saldoConta = saldoConta - valorSaque;
         }
         
         public void transferirValor(Conta contaDestino, Double valorTranferir){
-                contaDestino.saldoConta = contaDestino.saldoConta + valorTranferir;
-                saldoConta = saldoConta - valorTranferir;
+                
         }
 
         public void cancelarConta(){     
@@ -31,9 +29,6 @@ class Conta{
 
         // simula qual seria o montante final se o valor de saldoConta fosse investido por determinado numero de meses, a uma taxa específica
         public Double simularRendimento(Double jurosMes, Integer meses){
-                for(Integer i = 1; i <= meses; i++){
-                        saldoConta = saldoConta + saldoConta*jurosMes;
-                }
                 return saldoConta;
         }
 
